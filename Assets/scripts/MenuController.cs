cb6f826059390b61a46c43930c9ac0a3c4b41c5f
unstablesun <unstablesun@gmail.com>
2015-12-18 09:54:45 -0800

    added canvas popup

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuController : MonoBehaviour 
+{
+
+	[SerializeField]
+	protected Canvas canvasPopup1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+		canvasPopup1.gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
